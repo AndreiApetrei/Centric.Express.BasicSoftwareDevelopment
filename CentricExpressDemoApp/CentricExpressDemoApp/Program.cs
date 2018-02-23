d99a27a6c25cbd9bf010f13125763d32cf9c6925
radeanurazvan <radeanu.razvan99@gmail.com>
2018-02-24 01:35:38 +0200

    Added CentricExpressDemoApp folder

---
+++ CentricExpressDemoApp/CentricExpressDemoApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CentricExpressDemoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
